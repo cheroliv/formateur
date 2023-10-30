5991050c05584366b13704782309c73d3b73ca6b
cheroliv <cheroliv@laposte.net>
2023-10-30 22:00:37 +0100

    update app/gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,21 @@
-plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+buildscript {
 
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
-}
+    repositories {
+        google()
+        mavenCentral()
+    }
 
-repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
-}
-
-dependencies {
-    // Use the Kotlin JUnit 5 integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-
-    // Use the JUnit 5 integration.
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
-
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:32.1.1-jre")
-}
-
-// Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+    dependencies {
+        val jacksonVersion = "2.15.2"
+        classpath("org.slf4j:slf4j-simple:2.0.7")
+        classpath("commons-io:commons-io:2.13.0")
+        classpath("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+        classpath("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
+        classpath("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+        classpath("org.tukaani:xz:1.9")
     }
 }
 
-application {
-    // Define the main class for the application.
-    mainClass.set("learning.AppKt")
-}
-
-tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+repositories {
+    mavenCentral()
 }
