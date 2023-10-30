1fc23d27ab60bdf21a3ac33268f6b2919c96aa27
cheroliv <cheroliv@laposte.net>
2023-10-30 10:04:19 +0100

    add content

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "formateur_numérique"
+rootProject.name = "formateur_numerique"
 include("app")
