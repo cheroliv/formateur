6f545734dae547db719aae6dd0ec7608d92c67db
cheroliv <cheroliv@laposte.net>
2023-11-03 18:41:56 +0100

    update content: A2SP

--- lms/lms.py
+++ lms/lms.py
@@ -1,10 +1,13 @@
-import unittest
+def main():
+    print("Hello")
+    # # Connect to LibreOffice
+    # basic = uno.Basic()
+    # basic.connect("localhost", "localhost", 8100)
+    # if pip.is_package_installed("base"):
+    #     print("Le module `base` est installé.")
+    # else:
+    #     print("Le module `base` n'est pas installé.")
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertNotEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    main()
