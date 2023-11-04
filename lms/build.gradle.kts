8fd3ed8e090cbf400db730e16a7bc0a588c86ef8
cheroliv <cheroliv@laposte.net>
2023-11-04 05:09:12 +0100

    update content: A2SP

--- lms/build.gradle.kts
+++ lms/build.gradle.kts
@@ -1,3 +1,9 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+
+
 plugins {
     //https://github.com/avast/gradle-docker-compose-plugin
     id("com.avast.gradle.docker-compose") version ("0.17.5")
@@ -25,24 +31,95 @@ buildscript {
 }
 
 repositories { mavenCentral() }
+/*=================================================================================*/
+
+private val mapper: ObjectMapper by lazy {
+    ObjectMapper(YAMLFactory()).apply {
+        disable(WRITE_DATES_AS_TIMESTAMPS)
+        registerKotlinModule()
+    }
+}
+/*=================================================================================*/
 
 data class Formation(
-    val nom:String,
-    val spg:SPG
+    val nom: String,
+    val spg: SPG
 )
 
 /**
- * SPG: Scénario Pédagogique GLobal
+ * SPG: Scénario Pédagogique Global
  */
 data class SPG(
-    val theme:String,
-    val spd:SPD
+    val theme: String = "",
+    val titre: String = "",
+    val presentation: String = "",
+    val mindmap: String = "",
+    val publicProspect: String = "",
+    val prerequiz: String = "",
+    val objs: String = "",
+    val competences: String = "",
+    val timing: String = "",
+    val means: String = "",
+    val prgm: String = "",
+    val eval: String = "",
+    val certif: String = "",
+    val place: String = "",
+    val price: String = "",
+    val infra: String = "",
+    val pursuit: String = "",
+    val accessTime: String = "",
+    val mobility: String = "",
+    val testimony: String = "",
+    val testimonyCustomer: String = "",
+    val spd: SPD = SPD(),
+    val content: List<Triple<*, String, String>>? = listOf(
+        Triple(theme, "theme", "Thème"),
+        Triple(titre, "title", "Titre"),
+        Triple(presentation, "prez", "Présentation et description"),
+        Triple(mindmap, "mindmap", "Carte thématique"),
+        Triple(publicProspect, "public", "Public"),
+        Triple(prerequiz, "prerequiz", "Pré-requis et conditions d’accès à la formation (Qualiopi)"),
+        Triple(objs, "objs", "Objectifs pédagogiques (Qualiopi)"),
+        Triple(competences, "competences", "Compétences visées (Qualiopi)"),
+        Triple(timing, "timing", "Durée (Temporisation)] (Qualiopi)"),
+        Triple(means, "means", "Moyen d’accompagnement et Suivi pédagogique (Qualiopi)"),
+        Triple(
+            prgm,
+            "prgm",
+            "Programme pédagogique (Modalités pédagogiques)] (Qualiopi) : du contenu et du séquencement"
+        ),
+        Triple(eval, "eval", "Modalités d’évaluations] (Qualiopi)"),
+        Triple(certif, "certif", "Modalités de certification et Certification visé] (Qualiopi)"),
+        Triple(place, "place", "Lieux] (Qualiopi)"),
+        Triple(price, "price", "Tarifs"),
+        Triple(infra, "infra", "Moyens logistiques et matériels] (Qualiopi)"),
+        Triple(pursuit, "pursuit", "Poursuite en formation] (Qualiopi)"),
+        Triple(accessTime, "access_time", "Délais d’accès] (Réglementaire)"),
+        Triple(mobility, "mobility", "Accessibilité et Handicap] (Qualiopi)"),
+        Triple(testimony, "testimony", "Témoignage Evaluation de la formation] (Qualiopi)"),
+        Triple(testimonyCustomer, "testimony_customer", "Témoignage apprenant/commanditaire")
+    )
 )
 
 /**
  * SPD: Scénario Pédagogique Détaillé
  */
 data class SPD(
-    val titre:String,
-    val objectif:String
+    val titre: String="",
+    val objectif: String=""
 )
+/*=================================================================================*/
+
+tasks.register("spg") {
+    group = "FPA"
+    description = "Show SPG."
+    doFirst {
+        SPG().run {
+            println(mapper.writeValueAsString(this))
+            println(mapper.writeValueAsString(spd))
+        }
+    }
+}
+
+
+/*=================================================================================*/
