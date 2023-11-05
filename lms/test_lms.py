a5a227ff66952ef3b2c939e3dd78fb0645439952
cheroliv <cheroliv@laposte.net>
2023-11-05 18:31:02 +0100

    add content

--- lms/test_lms.py
+++ lms/test_lms.py
@@ -4,6 +4,13 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertNotEqual(True, False)  # add assertion here
+    # # Connect to LibreOffice
+    # basic = uno.Basic()
+    # basic.connect("localhost", "localhost", 8100)
+    # if pip.is_package_installed("base"):
+    #     print("Le module `base` est installé.")
+    # else:
+    #     print("Le module `base` n'est pas installé.")
 
 
 if __name__ == '__main__':
