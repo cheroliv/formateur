df8727b2e90cff93d32c1df3d61df3ed67ad826b
cheroliv <cheroliv@laposte.net>
2023-11-10 01:02:37 +0100

    clean, refactor, rename and add content.

---
+++ ECF/software-artisan/languages/python/functional.py
@@ -0,0 +1,25 @@
+# Python
+# first-class function
+# function closures
+# Recursion
+# HOF
+# libs :
+# pyrsistent
+
+# toolz
+# hypothesis
+# effect
+# py.test
+# nose
+# mock
+# mocker
+# pymock
+# sqlite3
+# json
+# pydantic
+
+
+
+
+
+# pypy stm, multiple thread
