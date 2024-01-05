ad879d8e1095f7622a71bf76dad9d4bb167e144f
cheroliv <cheroliv@laposte.net>
2024-01-05 03:24:53 +0100

    update training landing page

--- codes/training/next.config.js
+++ codes/training/next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+   //"basePath": "https://github.com/cheroliv/talaria.github.io.git",
+}
 module.exports = nextConfig
