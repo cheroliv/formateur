0c8d08131ee142e5d06d7e3e7a3c4ae0435a4f06
cheroliv <cheroliv@laposte.net>
2024-01-09 19:16:53 +0100

    Add gradle tasks to automate projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,36 @@ plugins {
     id("org.ajoberstar.grgit") version ("5.2.1")
 }
 
+//TODO :  task jbakeGhPagesTests
+tasks.register("jbakeGhPagesTests") {
+    group = "JbakeGhPages"
+    description = "Tests JbakeGhPages."
+    doLast { println(":jbakeGhPagesTests") }
+}
+
+//TODO :  task jbakeGhPagesInit
+tasks.register("jbakeGhPagesInit") {
+    group = "JbakeGhPages"
+    description = "Initialize a bake in a empty folder."
+    dependsOn("jbakeGhPagesTests")
+    doLast { println(":jbakeGhPagesInit") }
+}
+
+//TODO :  task jbakeGhPagesServe
+tasks.register("jbakeGhPagesServe") {
+    group = "JbakeGhPages"
+    description = "Run server to preview bake on localhost."
+    doLast { println(":jbakeGhPagesServe") }
+}
+
+//TODO :  task jbakeGhPagesPublish
+tasks.register("jbakeGhPagesPublish") {
+    group = "JbakeGhPages"
+    description = "Publish bake to github-pages."
+    dependsOn("jbakeGhPagesTests")
+    doLast { println(":jbakeGhPagesPublish") }
+}
+
 //TODO :  task jbakeGhPagesCommitAndPush
 tasks.register("jbakeGhPagesCommitAndPush") {
     group = "JbakeGhPages"
@@ -13,21 +43,35 @@ tasks.register("jbakeGhPagesCommitAndPush") {
 //        grgit.push()
     }
 }
-//TODO :  task jbakeGhPagesServer
-tasks.register("jbakeGhPagesServer") {
-    group = "JbakeGhPages"
-    description = "Run server to preview bake on localhost."
-    doLast { println(":jbakeGhPagesServer") }
+
+
+//TODO :  task talariaFrontendTests
+tasks.register("talariaFrontendTests") {
+    group = "Talaria"
+    description = "Test frontend."
+    doLast { println(":talariaFrontendTests") }
 }
-//TODO :  task jbakeGhPagesInit
-tasks.register("jbakeGhPagesInit") {
-    group = "JbakeGhPages"
-    description = "Initialize a bake in a empty folder."
-    doLast { println(":jbakeGhPagesInit") }
+
+//TODO :  task talariaFrontendPublish
+tasks.register("talariaFrontendPublish") {
+    group = "Talaria"
+    description = "Publish Talaria frontend to github-pages."
+    dependsOn("talariaFrontendTests")
+    doLast { println(":talariaFrontendPublish") }
 }
-//TODO :  task jbakeGhPagesPublish
-tasks.register("jbakeGhPagesPublish") {
-    group = "JbakeGhPages"
-    description = "Publish bake to github-pages."
-    doLast { println(":jbakeGhPagesPublish") }
+
+
+//TODO :  task talariaBackofficeTests
+tasks.register("talariaBackofficeTests") {
+    group = "Talaria"
+    description = "Test backoffice."
+    doLast { println(":talariaBackofficeTests") }
+}
+
+//TODO :  task talariaBackofficePush
+tasks.register("talariaBackofficePush") {
+    group = "Talaria"
+    description = "Push backoffice script to google workspace."
+    dependsOn("talariaBackofficeTests")
+    doLast { println(":talariaBackofficePush") }
 }
